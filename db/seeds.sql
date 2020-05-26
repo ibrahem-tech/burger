683565ab37734a7d18d5b1904b457459b1d46ed9
Your Name <ryibrahem1210@gmail.com>
2020-05-26 14:02:51 -0400

    database

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', FALSE);
